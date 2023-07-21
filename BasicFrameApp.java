f7e40e0f159712271a37c4b8b80f96391200b84e
FIBONACCI-MKHWEBANE <140148635+FIBONACCI-MKHWEBANE@users.noreply.github.com>
2023-07-21 09:22:31 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,14 @@
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp 
+{
+    public static void main(String[] args) 
+    {
+        MyFirstFrame fibo;
+        
+        fibo = new MyFirstFrame();
+    }
+    
+}
